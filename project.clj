6b71ce34048b8e716f97dea3f3cba307b3a7984a
Olivier Bohrer <olivier.bohrer@gmail.com>
2020-06-06 16:20:12 +0100

    release 0.8.0: amended package prefix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.viooh/kafka-ssl-helper "0.7.0"
+(defproject com.viooh/kafka-ssl-helper "0.8.0"
   :description "Temporary trust/keystores for your kafka apps"
   :url "https://github.com/VIOOH/kafka-ssl-helper"
 
